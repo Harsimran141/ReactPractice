ae165b194f06cfb38a658047c1d03f07745ae387
harsimran141 <harsimran141@gmail.com>
2026-08-07 18:11:59 -0400

    Remove Hobby component and associated props from App component

--- SecondApp/src/App.jsx
+++ SecondApp/src/App.jsx
@@ -1,15 +1,8 @@
-import Hobby from "./Hobby";
-
 function App(){
-  let hobbys = ["Reading","Writting","Gamming"];
   return(
     <div>
-      <Hobby name="Harsimranpal singh"
-              age={28}
-              city="Brampton"
-              hobbies={hobbys}
-      />
+
     </div>
-  );
+  )
 }
 export default App;
