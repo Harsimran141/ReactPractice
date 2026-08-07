e4e3f6894a3606fda08e9e31277c9b3da0dfc453
harsimran141 <harsimran141@gmail.com>
2026-08-07 10:07:49 -0400

    Add showAlert function and update App component structure

--- SecondApp/src/App.jsx
+++ SecondApp/src/App.jsx
@@ -1,12 +1,18 @@
 import Practice from "./Practice";
 
 function App(){
-  
+  const showAlert = ()=>{
+    alert("Hello waris singh");
+  }
   return(
     <div>
-
-   
-   
+<h1>Hello React </h1>
+   <p>Welcome to my website.</p>
+   <img src="https://media.istockphoto.com/id/833710244/photo/red-squirell.jpg?s=170667a&w=0&k=20&c=3uUjnCSJkDDMSZ6Xb_h6fefYxoFeya43QPkPH8XlWsM=" alt="placeholder Image" 
+   width ="300"
+   />
+   <br />
+   <button onClick={showAlert}>Click Pic</button>
     <Practice/>
     </div>
     
