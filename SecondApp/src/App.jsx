d61ddb6425576e43f94011698565e9af41f1ebbd
harsimran141 <harsimran141@gmail.com>
2026-08-07 15:37:04 -0400

    Refactor App component: remove User and Practice components, add Task component, and update structure for detail display

--- SecondApp/src/App.jsx
+++ SecondApp/src/App.jsx
@@ -1,23 +1,15 @@
 import { useState } from "react";
-
+import Task from "./Task";
 function App(){
-
-  const [name, setName] = useState("Harsimranpal singh");
-  const [count,setCount] = useState(0);
-  const nameChange = () =>{
-    setName("Waris singh Boparai")
-  }
+  const [show, setShow] = useState(false);
   return(
-   <div className="App">
-    <h1>Name:-{name}</h1>
-    <h1>Count={count}</h1>
-    <button onClick={nameChange}>Click Change Button</button>
-     <button onClick={() => setCount(count+1)}>Increment</button>
-     <button onClick={() =>{if(count > 0){setCount(count -1)}}}>Decrement</button>
-     <button onClick={()=> setCount(0)}>Reset</button>
-  </div>
-
-  
-  );
+    <div>
+      <h1>Waris singh Boparai (function name App)</h1>
+      <button onClick={()=> setShow(!show)}>
+        {show ? "Hide" : "show"}
+        Details</button>
+    {show && <Task/>}
+    </div>
+  )
 }
 export default App;
---
+++ SecondApp/src/Task.jsx
@@ -0,0 +1,8 @@
+function Task(){
+    return(
+        <div>
+            <h1>Jarmanjit singh Hundal(function name Task)</h1>
+        </div>
+    )
+}
+export default Task;
