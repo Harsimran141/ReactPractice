c20978cf301e1d68718d4f12cf2861166c7bd61d
harsimran141 <harsimran141@gmail.com>
2026-08-09 14:23:58 -0400

    refactor: improve Map component layout and fix typo in fruit list

--- SecondApp/src/Map.jsx
+++ SecondApp/src/Map.jsx
@@ -1,15 +1,20 @@
-function Map(){
-    const fruits =['Apple','Banana','Cherry','Pineapple','Watermelon'];
-    return(
-    <div className="flex justify-center ">
-        <h1 className="text-4xl font-bold text-green-800 ">Map function In React</h1>
-        <ul className="mt-10 text-xl text-white bg-green-600">
-            {fruits.map((fruit,index) => (
-                <li key={index}>{fruit}</li>
-            ))
-            }
-        </ul>
+
+function Map() {
+  const fruits = ["Apple", "Banana", "Cherry", "Pineapple", "Watermalon"];
+
+  return (
+    <div className="flex flex-col items-center">
+      <h1 className="text-4xl font-bold text-green-800">
+        Map Function In React
+      </h1>
+
+      <ul className="mt-10 text-xl br-rd-2px text-white bg-green-600 p-5">
+        {fruits.map((fruit, index) => (
+          <li key={index}>{fruit}</li>
+        ))}
+      </ul>
     </div>
-    )
+  );
 }
+
 export default Map;
