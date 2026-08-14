16c77142d502e327c94ab93ca5557e3abb11091f
harsimran141 <harsimran141@gmail.com>
2026-08-14 14:57:20 -0400

    refactor: streamline NetflixSeries component and reduce redundancy in App

--- SecondApp/src/App.jsx
+++ SecondApp/src/App.jsx
@@ -1,75 +1,40 @@
 function App() {
   return (
     <div>
-
-   
-    <div>
-      <h1 className="text-4xl font-bold text-blue-600">
-        Hello Tailwind CSS
-      </h1>
-
-      <img
-        src="/image/one.jpg"
-        alt="One"
-        className="w-[100px] mt-5"
-      />
-      <h1>Butterfly</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In dolorum asperiores nisi nostrum maxime, reiciendis cumque consectetur delectus accusantium reprehenderit magni aspernatur vel iusto, odit ipsum fugiat quaerat repellendus deleniti.</p>
-    </div>
-    <div>
-      <h1 className="text-4xl font-bold text-blue-600">
-        Hello Tailwind CSS
-      </h1>
-
-      <img
-        src="/image/one.jpg"
-        alt="One"
-        className="w-[100px] mt-5"
-      />
-      <h1>Butterfly</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In dolorum asperiores nisi nostrum maxime, reiciendis cumque consectetur delectus accusantium reprehenderit magni aspernatur vel iusto, odit ipsum fugiat quaerat repellendus deleniti.</p>
-    </div>
-    <div>
-      <h1 className="text-4xl font-bold text-blue-600">
-        Hello Tailwind CSS
-      </h1>
-
-      <img
-        src="/image/one.jpg"
-        alt="One"
-        className="w-[100px] mt-5"
-      />
-      <h1>Butterfly</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In dolorum asperiores nisi nostrum maxime, reiciendis cumque consectetur delectus accusantium reprehenderit magni aspernatur vel iusto, odit ipsum fugiat quaerat repellendus deleniti.</p>
-    </div>
-    <div>
-      <h1 className="text-4xl font-bold text-blue-600">
-        Hello Tailwind CSS
-      </h1>
-
-      <img
-        src="/image/one.jpg"
-        alt="One"
-        className="w-[100px] mt-5"
-      />
-      <h1>Butterfly</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In dolorum asperiores nisi nostrum maxime, reiciendis cumque consectetur delectus accusantium reprehenderit magni aspernatur vel iusto, odit ipsum fugiat quaerat repellendus deleniti.</p>
-    </div>
-    <div>
-      <h1 className="text-4xl font-bold text-blue-600">
-        Hello Tailwind CSS
-      </h1>
-
-      <img
-        src="/image/one.jpg"
-        alt="One"
-        className="w-[100px] mt-5"
-      />
-      <h1>Butterfly</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In dolorum asperiores nisi nostrum maxime, reiciendis cumque consectetur delectus accusantium reprehenderit magni aspernatur vel iusto, odit ipsum fugiat quaerat repellendus deleniti.</p>
-    </div>
-     </div>
+    <NetflixSeries/>
+    <NetflixSeries/>
+    <NetflixSeries/>
+    <NetflixSeries/>
+    <NetflixSeries/>
+    <NetflixSeries/>
+    <NetflixSeries/>
+    <NetflixSeries/>
+    <NetflixSeries/>
+    <NetflixSeries/>
+  </div>
   )
 }
 
-export default App
+export default App;
+const NetflixSeries = ()=>{
+    return (
+<div>
+    <div>
+      <h1 className="font-bold text-blue-900">
+        Hello Tailwind CSS
+      </h1>
+
+      <img
+        src="/image/one.jpg"
+        alt="One"
+        className="w-[100px] mt-5"
+      />
+      <h1>Butterfly</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+         In dolorum asperiores nisi nostrum maxime, reiciendis cumque
+          consectetur delectus accusantium reprehenderit magni 
+          aspernatur vel iusto, odit ipsum fugiat quaerat repellendus deleniti.</p>
+    </div>
+</div>
+    )
+}
