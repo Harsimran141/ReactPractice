ab8e207f0f73d08a1bfc1e1ffb09633157d0c2c0
harsimran141 <harsimran141@gmail.com>
2026-08-14 15:37:22 -0400

    feat: update Practice component to display student count and handle empty state

--- SecondApp/src/Practice.jsx
+++ SecondApp/src/Practice.jsx
@@ -1,9 +1,11 @@
 const Practice=()=>{
-const students =[];
+const students =[5,6];
 return(
     <>
 <h1>Display</h1>
-<p>{students.length && "No students found"}</p>
+{/* <p>{students.length  && "No students found"}</p> */}
+{/* <p>{students.length == 0  && "No students found"}</p> */}
+<p>{!students.length && "No students found"}</p>
 <p>Number of student {students.length}</p>
     </>
     )
