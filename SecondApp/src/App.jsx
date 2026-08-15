a46b4cd0ea09fbdc7aaa7abba01e6696b09e6884
harsimran141 <harsimran141@gmail.com>
2026-08-15 14:59:27 -0400

    feat: simplify App component by removing unused variables and returning a cleaner JSX structure

--- SecondApp/src/App.jsx
+++ SecondApp/src/App.jsx
@@ -1,30 +1,10 @@
 const App = () => {
-  let name ="Waris singh";
-  let arraydata = [10,20,30,45]; 
-  let record ={
-    "name":"Waris singh",
-    "city":"Amritsar",
-    "phone":"12345685"
-  }
-  let status = true;
-  return (
-    <div className="main">
-      Welcome React
-      <h1>{name}</h1>
-      {arraydata.map((value)=>{
-        return(
 
-          <div>{value}</div>
-        ) 
-      })}
-      <div>{record.city}</div>
-      <div>{record.name}</div>
-      <div>{record.phone}</div>
-      <h1 style={{color:"orange" , backgroundColor:"green"}}>
-        { (status)  ? "React App" : ""  }
-      </h1>
+  return (
+    <div>
+      <h1>React App</h1>
     </div>
-  );
+  )
 };
 
 export default App;
