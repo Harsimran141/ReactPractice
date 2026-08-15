a388fe40ae88c540ed251aeac254a3206bf5b873
harsimran141 <harsimran141@gmail.com>
2026-08-15 16:05:45 -0400

    feat: add Bootstrap and React-Bootstrap dependencies, update App and main components for improved layout

--- SecondApp/src/App.jsx
+++ SecondApp/src/App.jsx
@@ -1,21 +1,24 @@
 import Footer from "./Footer";
 import Header from "./Header";
-
+import {Col, Container, Row} from 'react-bootstrap'
 const App = () => {
 
   return (
     <div>
       {/* <h1>React App</h1> */}
       <Header/>
-      <Footer/>
-      <div className="row">
-       <Card />
-       <Card />
-       <Card />
-       <Card />
-       <Card />
-       <Card />
-      </div>
+<Container fluid>
+
+<Container>
+  <Row>
+    <Col className="col-12">
+    Welcome react
+    </Col>
+  </Row>
+</Container>
+</Container>
+       <Footer/>
+       <Card/>
 </div>
   )
 };
