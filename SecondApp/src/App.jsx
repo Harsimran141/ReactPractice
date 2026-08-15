94f200d05c54f5e55574e51e45b78a2256b12061
harsimran141 <harsimran141@gmail.com>
2026-08-15 14:29:03 -0400

    feat: refactor App component and update main.jsx to use App instead of Practice

--- SecondApp/src/App.jsx
+++ SecondApp/src/App.jsx
@@ -1,47 +1,18 @@
-function App() {
+const App = () => {
+  let name ="Waris singh";
+  let arraydata = [10,20,30,45]; 
   return (
-    <div>
-    <NetflixSeries/>
-    <NetflixSeries/>
-    <NetflixSeries/>
-    <NetflixSeries/>
-    <NetflixSeries/>
-    <NetflixSeries/>
-    <NetflixSeries/>
-    <NetflixSeries/>
-    <NetflixSeries/>
-    <NetflixSeries/>
-  </div>
-  )
-}
+    <div className="font-bold text-blue-900">
+      Welcome React
+      <h1>{name}</h1>
+      {arraydata.map((value)=>{
+        return(
+
+          <div>{value}</div>
+        ) 
+      })}
+    </div>
+  );
+};
 
 export default App;
-const NetflixSeries = ()=>{
-    const name = "Waris singh"
-    return (
-<div>
-    <div>
-        <h2>{name}</h2>
-      <h1 className="font-bold text-blue-900">
-        Hello Tailwind CSS
-      </h1>
-    
-      <img
-        src="/image/one.jpg"
-        alt="One"
-        className="w-[100px] mt-5"
-      />
-      <h1>Butterfly</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-         In dolorum asperiores nisi nostrum maxime, reiciendis cumque
-          consectetur delectus accusantium reprehenderit magni 
-          aspernatur vel iusto, odit ipsum fugiat quaerat repellendus deleniti.</p>
-    </div>
-    <h2>Name{name}</h2>
-    <h3>Rating: {name}</h3>
-  <button className="p-[5px] text-red-500">
-  Watch Video
-</button>
-</div>
-    )
-}
--- SecondApp/src/main.jsx
+++ SecondApp/src/main.jsx
@@ -2,13 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import './index.css'
-// import App from './App.jsx'
-import Practice from './Practice.jsx'
+import App from './App'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
- {/* \<App/> */}
+<App/>
   
- <Practice/>
+
   </StrictMode>,
 )
