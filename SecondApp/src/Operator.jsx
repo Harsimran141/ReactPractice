8f3bf85dee7226781cc312e311ccde12f10bf2a3
harsimran141 <harsimran141@gmail.com>
2026-08-16 08:43:28 -0400

    feat: implement Operator component with toggle display functionality

--- SecondApp/src/Operator.jsx
+++ SecondApp/src/Operator.jsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
+
 const Operator =()=>{
+    const [show,setShow]=useState(false);
     return(
         <div>
-            <h1>Operator</h1>
+            <h1>Display</h1>
+            <button className="bg-[green] text-white font-bold p-2 rounded shadow-lg">Show</button>
+       { (show) ?
+     <p>Waris Singh Boparai</p>
+     :
+     ''
+    }
+
+
         </div>
     )
 }
