234fd78e38fb8ae000a362e78e54889709501cb0
harsimran141 <harsimran141@gmail.com>
2026-08-16 07:28:09 -0400

    feat: enhance App component by adding state management and improving button functionality

--- SecondApp/src/App.jsx
+++ SecondApp/src/App.jsx
@@ -1,14 +1,20 @@
+import { useState } from "react";
 
 const App = () => {
+  
 let Display=()=>{
   alert("Welcome")
 }
+let n=10;
 let Addnumber =(a,b)=>{
   console.log(a+b);
+  n=n+1;
+  console.log(n);
   
 }
   return (
     <div>
+   
 <h2>React App </h2>
    <button onClick={Display} className="bg-[red] px-2 py-2 rounded text-white border-1px shadow-lg border-rarious-2px">Add</button>  
   <button onClick={()=>Addnumber(2,4)} className="bg-[blue] text-white rounded px-4"  >AddNum</button>
