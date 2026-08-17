ee0157ad7a09328fe798539077cc56cd485d86ed
harsimran141 <harsimran141@gmail.com>
2026-08-17 16:11:30 -0400

    fix: ensure task input is cleared after adding a todo and improve rendering structure

--- SecondApp/src/Todoapp.jsx
+++ SecondApp/src/Todoapp.jsx
@@ -8,6 +8,7 @@ function Todoapp() {
         return;
     }
     setTodos([...todos,task])
+    setTask("");
  }
  const editTodo = (index)=> {
     const newTask = prompt("Enter new task");
@@ -24,26 +25,38 @@ setTodos(updateTodos);
     <div className=" bg-green-900 w-56 m-auto h-56 rounded-md justify-center mt-5">
     <h1 className="text-center">My ToDo App</h1>
     <div className="flex gap-1 justify-center ">
-     <input type="text" onChange={(e)=>setTask(e.target.value)} className="bg-amber-50 w-38 rounded border border-none" />
+     <input type="text" onChange={(e)=>setTask(e.target.value)} value={task} className="bg-amber-50 w-38 rounded border border-none" />
      <button className="bg-red-300 shadow-2xs rounded p-0.5 text-white font-bold hover:bg-green-600"
      onClick={addTodo}
      >Add</button>
    
     </div>
 <div>
-    {todos.map((value,index)=>{
-        return(<p className="text-white" key={index}>{value}
-        <div className="flex gap-1">
+  {todos.map((value, index) => {
+    return (
+      <div className="text-white" key={index}>
+        <span>{value}</span>
 
-        <button onClick={()=>setTodos(todos.filter((items,i)=>i !==index))} 
-        className="bg-red-600 rounded hover:text-black p-0.5  
-        font-bold shadow-2xl">Del</button>
-        <button onClick={()=>editTodo(index)} className="bg-green-600 rounded font-bold
-         p-0.5 hover:bg-amber-300 text-blue-800">Edit</button>
+        <div className="flex gap-1 mt-1">
+          <button
+            onClick={() =>
+              setTodos(todos.filter((items, i) => i !== index))
+            }
+            className="bg-red-600 rounded hover:text-black p-0.5 font-bold shadow-2xl"
+          >
+            Del
+          </button>
+
+          <button
+            onClick={() => editTodo(index)}
+            className="bg-green-600 rounded font-bold p-0.5 hover:bg-amber-300 text-blue-800"
+          >
+            Edit
+          </button>
         </div>
-        </p>)
-
-    })}
+      </div>
+    );
+  })}
 </div>
     </div>
   )
